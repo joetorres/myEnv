e09e1a304d2b75db5a4b4be982c73f72432d75cc
Jerônimo Torres <jeronimo.torres@outlook.com>
2022-07-22 07:44:31 -0700

    fixing package list

--- start.wsl.sh
+++ start.wsl.sh
@@ -5,8 +5,6 @@ sudo apt-get upgrade -y
 
 #instalações básicas de dependencias
 sudo apt-get install -y \
-    firefox \
-    golang \
     software-properties-common \
     apt-utils \
     vim \
@@ -15,35 +13,42 @@ sudo apt-get install -y \
     npm \
     python3-pip \
     wget curl \
+    firefox \
     ranger \
     taskwarrior \
     ssh \
-    chromium-browser \
     cmake \
+    default-jdk \
+    build-essential \
+    vim-nox \
+    python3-dev \
+    mono-complete \
     gcc \
     gdb \
     gpp \
     g++ \
+    golang \
     htop \
     tmux \
     net-tools \
     neofetch \
+    deluge \
     apt-transport-https \
     gnupg-agent \
     ca-certificates \
     gitk \
     mpv \
     snapd \
-    rclone \
-    lua5.3
+    rclone
     
+
 
 #config git
 git config --global user.email "jeronimo.torres@outlook.com"
 git config --global user.name "Jerônimo Torres"
 git config --global credential.helper store
 git config --global core.editor "vim"
-
+git config pull.rebase false 
 
 
 #dotnet
@@ -53,21 +58,28 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
 sudo apt-get install dotnet-sdk-6.0 -y
 
+#php 
+sudo apt install php php-common php-mysql php-pgsql php-zip php-bz2 php-composer-ca-bundle php-bcmath php-curl php-cgi php-date php-mbstring php-uuid phpunit -y
+
+
+#video stuff
+sudo pip3 install subliminal
+sudo pip3 install virtualenv
+echo -e '\n' >> ~/.bashrc
+echo 'alias sub="subliminal download -l pt-br ."' >> ~/.bashrc
+
 
 #config de SSH
-ssh-keygen
-
+cp config ~/.ssh
 
 #node
-sudo npm i -g npm 
-sudo npm i -g n 
-sudo n lts
-sudo rm /usr/bin/nodejs
-sudo ln -s /usr/local/bin/node /usr/bin/nodejs
-
+#install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install --lts
+nvm use --lts
 
 #vim
-cp ./dotconfig/vimrc ~/.vimrc
+cp vimrc ~/.vimrc
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
@@ -75,3 +87,9 @@ git clone https://github.com/ycm-core/YouCompleteMe.git ~/.vim/bundle/YouComplet
 cd ~/.vim/bundle/YouCompleteMe
 git submodule update --init --recursive
 python3 install.py --all
+
+# instalando platformio e arduino
+pip install platformio 
+curl -fsSL https://raw.githubusercontent.com/platformio/platformio-core/master/scripts/99-platformio-udev.rules | sudo tee /etc/udev/rules.d/99-platformio-udev.rules
+sudo service udev restart
+
